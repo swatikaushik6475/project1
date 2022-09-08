7ef7782beccf4da78c0f5cd61abdbb8f178ca5fa
swatikaushik6475 <76221287+swatikaushik6475@users.noreply.github.com>
2022-09-08 15:16:28 +0530

    Add files via upload

---
+++ swati.js
@@ -0,0 +1,17 @@
+let c=prompt("Enter your first name");
+let b=prompt("Enter your second name");
+alert("Hi nice to meet you "+c+" "+b);
+let a=1;
+switch (a) {
+ 
+    case 1:
+        a = 'one';
+        break;
+    case 2:
+        a = 'two';
+        break;
+    default:
+        a = 'not found';
+        break;
+}
+console.log(`The value is ${a}`);
